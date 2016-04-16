a956e17860aed8b85084b18603721d84118e74c4
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2016-04-16 15:38:03 +0200

    Print affiliation of current sample in ranking

--- Code/get_website_rankings.py
+++ Code/get_website_rankings.py
@@ -7,6 +7,15 @@ from itertools import product
 from string import Template
 
 
+def get_aff(aff_list):
+    """Return unique affiliation(s) from pandas series."""
+    a = [aff for aff in aff_list if aff != '']
+    if len(a) > 0:
+        return "; ".join(set(a))
+    else:
+        return ""
+
+
 def get_linktext(node, nw, tp):
     """"Return string with link depending on network definition."""
     if nw == "comwith":
@@ -33,21 +42,21 @@ def write_out_ranking(rank_dct, file, nw, tp, measure):
     <tr>
         <th>Rank</th>
         <th>Name</th>
-        <th>Affiliation (in 2011, if in data)</th>
+        <th>Affiliation (on published papers)</th>
         <th>Value</th>
     </tr>
 </thead>
 <tbody>
 """
+    t = Template('<td>$rank</td><td>$text</td><td>$aff</td><td>$val</td>')
     table = []
     for node, sub in rank_dct.items():
         # Get elements
         rank = sub[measure + '_rank']
         text = get_linktext(node, nw, tp)
-        aff = sub["affiliation"]
+        aff = sub["affiliation"][tp]
         value = round_if_needed(rank_dct[node][measure], measure)
         # Combine elements
-        t = Template('<td>$rank</td><td>$text</td><td>$aff</td><td>$val</td>')
         entry = t.substitute(rank=rank, text=text, aff=aff, val=value)
         table.append('<tr>' + entry + '</tr>')
     out_text = header + '\n'.join(table) + "\n</tbody>"
@@ -58,11 +67,14 @@ def write_out_ranking(rank_dct, file, nw, tp, measure):
 if __name__ == '__main__':
     # VARIABLES
     IC = "../../InformalCollaboration/Code/"  # relative path of IC repository
-    timepoints = ["early", "late"]
     networks = ["comwith", "auth"]
     input_folder = IC + "211_centralities/"
     affiliation_file = IC + "112_node_lists/all-all_authors.csv"
     output_folder = "../templates/rankings/"
+
+    year_map = {"early": range(1998, 2000+1),
+                "late": range(2009, 2011+1)}
+    timepoints = year_map.keys()
 
     combs = product(timepoints, networks)
     for (tp, nw) in combs:
@@ -76,16 +88,17 @@ if __name__ == '__main__':
             ranks = {row["node"]: row for row in reader}
         with open(affiliation_file, 'r') as f:
             reader = csv.DictReader(f)
-            affs = {row["author"]: row["2011"] for row in reader}
+            affs = {row["author"]: row for row in reader}
         # Remove unneccessary information; merge affiliations
         for author, subdict in ranks.items():
             for key in subdict.keys():
                 if not any(variable in key for variable in keeps):
                     del subdict[key]
+            subdict["affiliation"] = d = dict.fromkeys(timepoints, "")
             if author in affs:
-                subdict["affiliation"] = affs[author]
-            else:
-                subdict["affiliation"] = ""
+                for tp, years in year_map.items():
+                    cur_affs = [affs[author][str(y)] for y in year_map[tp]]
+                    d.update({tp: get_aff(cur_affs)})
 
         # SORT AND WRITE OUT
         for measure in keeps[1:]:
